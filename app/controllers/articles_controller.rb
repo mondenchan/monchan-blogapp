f3f720a157b12d848e7a4d51b165e03452ae6a41
monchan <monden2016@gmail.com>
2025-08-02 12:26:47 +0900

    ヘッダーに画像を表示　 CSSファイルを作成 記事UIを作成 article controller に変更

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,9 @@
-class HomeController < ApplicationController
+class ArticlesController < ApplicationController
   def index
     @article = Article.first
 
     # This action will render the home page of the application.
     # You can add any logic here if needed.
   end
-  def about
-  end
+
 end
