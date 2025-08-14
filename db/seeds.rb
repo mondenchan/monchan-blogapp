51a60e12985502fd9a43ba58c0eee45eef203da1
monchan <monden2016@gmail.com>
2025-08-14 13:09:28 +0900

    ユーザーと記事を紐づけ データベースのロールバックとマイグレーション author_ name のリファクタリング seedファイルの修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,20 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+john = User.create!(email: 'john@example.com', password: 'password')
+emily = User.create!(email: 'emily@example.com', password: 'password')
 
-10.times do
-Article.create(
+
+5.times do
+john.articles.create(
   title: Faker::Lorem.sentence(word_count: 5),
   content: Faker::Lorem.sentence(word_count: 100)
 )
 end
+5.times do
+emily.articles.create(
+  title: Faker::Lorem.sentence(word_count: 5),
+  content: Faker::Lorem.sentence(word_count: 100)
+)
+end
+
